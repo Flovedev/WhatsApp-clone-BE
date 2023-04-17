0e75b9197aa3b7103d8d32de6bb1b9b75eeb583a
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-04-17 16:57:39 +0200

    oauth changed

--- src/lib/auth/googleOauth.ts
+++ src/lib/auth/googleOauth.ts
@@ -1,11 +1,13 @@
-import GoogleStrategy from "passport-google-oauth20";
-import UsersModel from "../../users/model.js";
+import passportGoogle from "passport-google-oauth20";
+import UsersModel from "../../api/users/model";
 import { createAccessToken } from "./tools.js";
+
+const GoogleStrategy = passportGoogle.Strategy;
 
 const googleStrategy = new GoogleStrategy(
   {
-    clientID: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
+    clientID: process.env.GOOGLE_ID!,
+    clientSecret: process.env.GOOGLE_SECRET!,
     callbackURL: `${process.env.API_URL}/users/googleRedirect`,
   },
   async (_, __, profile, passportNext) => {
@@ -13,6 +15,7 @@ const googleStrategy = new GoogleStrategy(
     // Here we are going to receive some informations about the user from Google (scopes --> profile, email)
     try {
       const { email, given_name, family_name, sub } = profile._json;
+      const username = given_name! + " " + family_name!;
       console.log("PROFILE:", profile);
       // 1. Check if the user is already in db
       const user = await UsersModel.findOne({ email });
@@ -20,7 +23,6 @@ const googleStrategy = new GoogleStrategy(
         // 2. If he is there --> generate an accessToken (optionally also a refreshToken)
         const accessToken = await createAccessToken({
           _id: user._id,
-          role: user.role,
         });
 
         // 2.1 Then we can go next (to /googleRedirect route handler function)
@@ -28,8 +30,7 @@ const googleStrategy = new GoogleStrategy(
       } else {
         // 3. If user is not in our db --> create that
         const newUser = new UsersModel({
-          firstName: given_name,
-          lastName: family_name,
+          username,
           email,
           googleId: sub,
         });
@@ -39,7 +40,6 @@ const googleStrategy = new GoogleStrategy(
         // 3.1 Then generate an accessToken (optionally also a refreshToken)
         const accessToken = await createAccessToken({
           _id: createdUser._id,
-          role: createdUser.role,
         });
 
         // 3.2 Then we go next (to /googleRedirect route handler function)

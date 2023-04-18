e4e01d69bcc2efc765c23061f07f6e652226c2f7
Flovedev <flove.dev@gmail.com>
2023-04-18 13:59:56 +0100

    socket.io not working

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,5 +1,30 @@
 import { Socket } from "socket.io";
+import { UserIo } from "../interfaces/ISocketIo";
+
+let onlineUsers = <UserIo[]>[];
 
 export const newConnectionHandler = (socket: Socket) => {
   console.log("A new client connected! Id:", socket.id);
+
+  socket.emit("welcome", { message: `Hello, ${socket.id}` });
+
+  socket.on("setUsername", (payload) => {
+    console.log(payload);
+
+    onlineUsers.push({ username: payload.username, socketId: socket.id });
+
+    socket.emit("loggedIn", onlineUsers);
+
+    socket.broadcast.emit("updateOnlineUserList", onlineUsers);
+  });
+
+  socket.on("sendMessage", (message) => {
+    socket.broadcast.emit("newMessage", message);
+  });
+
+  // socket.on("disconnect", () => {
+  //   onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+
+  //   socket.broadcast.emit("updateOnlineUsersList", onlineUsers);
+  // });
 };

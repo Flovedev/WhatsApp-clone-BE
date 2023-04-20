de606ae86be7c2044742e5b4dc46768f3afd6c32
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-04-20 14:26:49 +0200

    info added to user

--- src/api/users/model.ts
+++ src/api/users/model.ts
@@ -1,55 +1,59 @@
-import mongoose from "mongoose"
-import bcrypt from "bcrypt"
-import { UserDocument, UserModel } from "../../interfaces/IUser"
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { UserDocument, UserModel } from "../../interfaces/IUser";
 
-const { Schema, model } = mongoose
+const { Schema, model } = mongoose;
 
 const UsersSchema = new Schema(
-    {
-        username: { type: String, required: true },
-        email: { type: String, required: true },
-        password: { type: String, required: false },
-        avatar: { type: String, default: "https://www.transparentpng.com/thumb/user/blue-male-user-profile-transparent-png-2lbgMx.png" },
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: false },
+    info: { type: String, default: "I am using what's app!" },
+    avatar: {
+      type: String,
+      default:
+        "https://www.transparentpng.com/thumb/user/blue-male-user-profile-transparent-png-2lbgMx.png",
     },
-    { timestamps: true }
-)
+  },
+  { timestamps: true }
+);
 
 UsersSchema.pre("save", async function () {
-    const newUserData = this
+  const newUserData = this;
 
-    if (newUserData.isModified("password")) {
-        const plainPW = newUserData.password
+  if (newUserData.isModified("password")) {
+    const plainPW = newUserData.password;
 
-        const hash = await bcrypt.hash(plainPW!, 11)
-        newUserData.password = hash
-    }
-})
+    const hash = await bcrypt.hash(plainPW!, 11);
+    newUserData.password = hash;
+  }
+});
 
 UsersSchema.methods.toJSON = function () {
-    const currentUserDocument = this
-    const currentUser = currentUserDocument.toObject()
-    delete currentUser.password
-    delete currentUser.createdAt
-    delete currentUser.updatedAt
-    delete currentUser.__v
-    return currentUser
-}
+  const currentUserDocument = this;
+  const currentUser = currentUserDocument.toObject();
+  delete currentUser.password;
+  delete currentUser.createdAt;
+  delete currentUser.updatedAt;
+  delete currentUser.__v;
+  return currentUser;
+};
 
 UsersSchema.static("checkCredentials", async function (email, plainPW) {
+  const user = await this.findOne({ email });
 
-    const user = await this.findOne({ email })
+  if (user) {
+    const passwordMatch = await bcrypt.compare(plainPW, user.password);
 
-    if (user) {
-        const passwordMatch = await bcrypt.compare(plainPW, user.password)
-
-        if (passwordMatch) {
-            return user
-        } else {
-            return null
-        }
+    if (passwordMatch) {
+      return user;
     } else {
-        return null
+      return null;
     }
-})
+  } else {
+    return null;
+  }
+});
 
-export default model<UserDocument, UserModel>("user", UsersSchema)
+export default model<UserDocument, UserModel>("user", UsersSchema);
--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,14 +1,18 @@
-import { Model, Document } from "mongoose"
+import { Model, Document } from "mongoose";
 
 interface User {
-    username: string
-    email: string,
-    password: string,
-    avatar?: string
+  username: string;
+  email: string;
+  password: string;
+  info: string;
+  avatar?: string;
 }
 
-export interface UserDocument extends User, Document { }
+export interface UserDocument extends User, Document {}
 
 export interface UserModel extends Model<UserDocument> {
-    checkCredentials(email: string, password: string): Promise<UserDocument | null>
+  checkCredentials(
+    email: string,
+    password: string
+  ): Promise<UserDocument | null>;
 }
